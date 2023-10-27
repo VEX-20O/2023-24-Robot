a6466a67b062d7bbcbc8805498fe1ec3f2fbd4ca
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-10-26 23:06:13 -0700

    Update main.cpp

--- 20V Robot Code/src/main.cpp
+++ 20V Robot Code/src/main.cpp
@@ -21,92 +21,46 @@
 using namespace vex;
 competition Competition;
 
-/*---------------------------------------------------------------------------*/
-/*                             VEXcode Config                                */
-/*                                                                           */
-/*  Before you do anything else, start by configuring your motors and        */
-/*  sensors using the V5 port icon in the top right of the screen. Doing     */
-/*  so will update robot-config.cpp and robot-config.h automatically, so     */
-/*  you don't have to. Ensure that your motors are reversed properly. For    */
-/*  the drive, spinning all motors forward should drive the robot forward.   */
-/*---------------------------------------------------------------------------*/
-
-/*---------------------------------------------------------------------------*/
-/*                             JAR Config                           */
-/*                                                                           */
-/*  Where all the magic happens. Follow the instructions below to input      */
-/*  all the physical constants and values for your robot. You should         */
-/*  already have configured your robot manually with the sidebar configurer. */
-/*---------------------------------------------------------------------------*/
-
 Drive chassis(
-
-//Specify your drive setup below. There are seven options:
-//ZERO_TRACKER_NO_ODOM, ZERO_TRACKER_ODOM, TANK_ONE_ENCODER, TANK_ONE_ROTATION, TANK_TWO_ENCODER, TANK_TWO_ROTATION, HOLONOMIC_TWO_ENCODER, and HOLONOMIC_TWO_ROTATION
-//For example, if you are not using odometry, put ZERO_TRACKER_NO_ODOM below:
 ZERO_TRACKER_ODOM,
-
-//Add the names of your Drive motors into the motor groups below, separated by commas, i.e. motor_group(Motor1,Motor2,Motor3).
-//You will input whatever motor names you chose when you configured your robot using the sidebar configurer, they don't have to be "Motor1" and "Motor2".
-
 //Left Motors:
 motor_group(DriveL1,DriveL2,DriveL3),
-
 //Right Motors:
 motor_group(DriveR1,DriveR2,DriveR3),
 
-//Specify the PORT NUMBER of your inertial sensor, in PORT format (i.e. "PORT1", not simply "1"):
+//PORT NUMBER of inertial
 PORT6,
 
-//Input your wheel diameter. (4" omnis are actually closer to 4.125"):
+//Wheel Dia
 3.25,
 
-//External ratio, must be in decimal, in the format of input teeth/output teeth.
-//If your motor has an 84-tooth gear and your wheel has a 60-tooth gear, this value will be 1.4.
-//If the motor drives the wheel directly, this value is 1:
+//External ratio (in/out)
 0.66,
 
-//Gyro scale, this is what your gyro reads when you spin the robot 360 degrees.
-//For most cases 360 will do fine here, but this scale factor can be very helpful when precision is necessary.
+//gyro 
 360,
 
-/*---------------------------------------------------------------------------*/
-/*                                  PAUSE!                                   */
-/*                                                                           */
-/*  The rest of the drive constructor is for robots using POSITION TRACKING. */
-/*  If you are not using position tracking, leave the rest of the values as  */
-/*  they are.                                                                */
-/*---------------------------------------------------------------------------*/
+//ONLY need to adjust the FORWARD TRACKER CENTER DISTANCE.
 
-//If you are using ZERO_TRACKER_ODOM, you ONLY need to adjust the FORWARD TRACKER CENTER DISTANCE.
-
-//FOR HOLONOMIC DRIVES ONLY: Input your drive motors by position. This is only necessary for holonomic drives, otherwise this section can be left alone.
+//Leave this alone
 //LF:      //RF:    
 PORT1,     -PORT2,
 
 //LB:      //RB: 
 PORT3,     -PORT4,
 
-//If you are using position tracking, this is the Forward Tracker port (the tracker which runs parallel to the direction of the chassis).
-//If this is a rotation sensor, enter it in "PORT1" format, inputting the port below.
-//If this is an encoder, enter the port as an integer. Triport A will be a "1", Triport B will be a "2", etc.
 3,
 
-//Input the Forward Tracker diameter (reverse it to make the direction switch):
 2.75,
 
-//Input Forward Tracker center distance (a positive distance corresponds to a tracker on the right side of the robot, negative is left.)
 //For a zero tracker tank drive with odom, put the positive distance from the center of the robot to the right side of the drive.
 //This distance is in inches:
 4,
 
-//Input the Sideways Tracker Port, following the same steps as the Forward Tracker Port:
 1,
 
-//Sideways tracker diameter (reverse to make the direction switch):
 -2.75,
 
-//Sideways tracker center distance (positive distance is behind the center of the robot, negative is in front):
 5.5
 
 );
