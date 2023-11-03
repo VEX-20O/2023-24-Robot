cf5d9e08bff5ad1c9755fc42b8eb8445904118aa
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-11-03 10:28:11 -0700

    Finished Adding Auto Selection Images

--- 20V Robot Code/src/BrainDrawAuto.cpp
+++ 20V Robot Code/src/BrainDrawAuto.cpp
@@ -1,7 +1,8 @@
 #include "vex.h"
 
-//480x272
-  //starting bottom x coord, side length, color, starting bottom y coord
+//Brain Screen is 480x272 pixels, origin is top left
+
+//starting bottom x coord, side length, color, starting bottom y coord
 void DrawDiamond(int bl,int leng, color col, int y){
   int rx, ry, lx, ly, tmx, tmy;
   rx = bl+leng*cos(to_rad(45));
@@ -15,7 +16,6 @@ void DrawDiamond(int bl,int leng, color col, int y){
   Brain.Screen.drawLine(rx,ry,tmx,tmy);
   Brain.Screen.drawLine(tmx,tmy,lx,ly);
   Brain.Screen.drawLine(lx,ly,bl,y);
-
 }
 
 void DrawField(){
@@ -24,12 +24,12 @@ void DrawField(){
   Brain.Screen.drawRectangle(50, 200, 15, -100);
   Brain.Screen.drawLine(65, 200-50, 50+15+50, 200);
   Brain.Screen.drawRectangle(480-100+50, 200, -15, -100);
-  Brain.Screen.drawLine(480-65, 200-50, 480-150, 200);
+  Brain.Screen.drawLine(480-65, 200-50, 480-100-15, 200);
   Brain.Screen.drawRectangle(150, 150, 480-300, 15);
 }
 void DrawAuto1(){DrawField();
   Brain.Screen.setPenColor(red);
-  Brain.Screen.drawRectangle(115, 180, 30, 30);
+  Brain.Screen.drawRectangle(115, 200, 30, -30);
   Brain.Screen.setPenColor(white);
 }
 void DrawAuto2(){DrawField();
@@ -40,17 +40,41 @@ void DrawAuto2(){DrawField();
   Brain.Screen.setPenColor(white);
 }
 void DrawAuto3(){DrawField();
-
+  Brain.Screen.setPenColor(red);
+  Brain.Screen.drawRectangle(115, 200, 30, -30);
+  Brain.Screen.setPenColor(green);
+  Brain.Screen.drawRectangle(50+15, 150, 16, 16);
+  Brain.Screen.drawRectangle(100, 200-15, 15, -15);
+  Brain.Screen.setPenColor(white);
 }
 void DrawAuto4(){DrawField();
-
+  DrawDiamond(115, 30, red, 200);
+  DrawDiamond(115,16,green,200-12);
+  Brain.Screen.setPenColor(green);
+  Brain.Screen.drawRectangle(50+15, 150, 16, 16);
+  Brain.Screen.setPenColor(white);
 }
 void DrawAuto5(){DrawField();
-
+  Brain.Screen.setPenColor(red);
+  Brain.Screen.drawRectangle(115, 200, 30, -30);
+  Brain.Screen.setPenColor(green);
+  Brain.Screen.drawRectangle(50+15+50, 200, 16, -16);
+  Brain.Screen.drawRectangle(115, 200-15, 15, -15);
+  Brain.Screen.setPenColor(white);
 }
 void DrawAuto6(){DrawField();
-
+  Brain.Screen.setPenColor(red);
+  Brain.Screen.drawRectangle(300, 195, 30, -30);
+  Brain.Screen.setPenColor(green);
+  Brain.Screen.drawRectangle(50+15+200, 150, 16, 16);
+  Brain.Screen.drawRectangle(250, 200-15, 15, -15);
+  Brain.Screen.setPenColor(white);
 }
 void DrawAuto7(){DrawField();
-
+  Brain.Screen.setPenColor(red);
+  Brain.Screen.drawRectangle(300, 195, 30, -30);
+  Brain.Screen.setPenColor(green);
+  Brain.Screen.drawRectangle(50+15+200, 150, 16, 16);
+  Brain.Screen.drawRectangle(250, 200-15, 15, -15);
+  Brain.Screen.setPenColor(white);
 }
